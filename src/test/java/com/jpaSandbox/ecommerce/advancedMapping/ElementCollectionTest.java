e17a20a02748ec4d1543d62acea564849a8510c3
AnaKlara <claracorreadasilva@gmail.com>
2025-04-16 14:35:41 -0300

    Mapeia coleção de objetos embutidos

--- src/test/java/com/jpaSandbox/ecommerce/advancedMapping/ElementCollectionTest.java
+++ src/test/java/com/jpaSandbox/ecommerce/advancedMapping/ElementCollectionTest.java
@@ -1,6 +1,7 @@
 package com.jpaSandbox.ecommerce.advancedMapping;
 
 import com.jpaSandbox.ecommerce.EntityManagerTest;
+import com.jpaSandbox.ecommerce.model.Atributo;
 import com.jpaSandbox.ecommerce.model.Produto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assertions.assertFalse(produtoVerificacao.getTags().isEmpty());
+    }
+
+    @Test
+    public void aplicarAtributos() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertFalse(produtoVerificacao.getAtributos().isEmpty());
     }
 }
