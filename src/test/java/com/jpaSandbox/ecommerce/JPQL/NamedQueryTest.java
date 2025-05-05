eb483ce5a157c02dfac9fab242ed21ce15f54fe8
AnaKlara <claracorreadasilva@gmail.com>
2025-05-05 12:20:30 -0300

    Consultas com Named queries

--- src/test/java/com/jpaSandbox/ecommerce/JPQL/NamedQueryTest.java
+++ src/test/java/com/jpaSandbox/ecommerce/JPQL/NamedQueryTest.java
@@ -1,9 +1,8 @@
 package com.jpaSandbox.ecommerce.JPQL;
 
 import com.jpaSandbox.ecommerce.EntityManagerTest;
+import com.jpaSandbox.ecommerce.model.Pedido;
 import com.jpaSandbox.ecommerce.model.Produto;
-import jakarta.persistence.NamedQueries;
-import jakarta.persistence.NamedQuery;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,6 +10,36 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.todos", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoPedido() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.todos", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXML() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.listar", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void executarConsulta() {
