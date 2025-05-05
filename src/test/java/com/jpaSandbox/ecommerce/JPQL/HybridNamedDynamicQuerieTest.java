26e4b11972740f1aefdc9f09e3951c1b26858698
AnaKlara <claracorreadasilva@gmail.com>
2025-05-05 12:30:15 -0300

    Consultas com abordagem híbrida: Named + dynamic queries

---
+++ src/test/java/com/jpaSandbox/ecommerce/JPQL/HybridNamedDynamicQuerieTest.java
@@ -0,0 +1,37 @@
+package com.jpaSandbox.ecommerce.JPQL;
+
+import com.jpaSandbox.ecommerce.EntityManagerTest;
+import com.jpaSandbox.ecommerce.model.Categoria;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class HybridNamedDynamicQuerieTest extends EntityManagerTest {
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        String jpql = "select c from Categoria c";
+        TypedQuery<Categoria> typedQuery = em.createQuery(jpql, Categoria.class);
+
+        entityManagerFactory.addNamedQuery("Categoria.listar", typedQuery);
+    }
+
+    @Test
+    public void usarAbordagemHibrida(){
+        TypedQuery<Categoria> typedQuery = entityManager
+                .createNamedQuery("Categoria.listar", Categoria.class);
+
+        List<Categoria> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+}
